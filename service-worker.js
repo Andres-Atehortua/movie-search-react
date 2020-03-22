fd9462ce793a8ce5a24512e23be3855f801f421a
Andres Atehortua <anderson17colombia@hotmail.com>
2020-03-22 22:58:44 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-search-react/precache-manifest.a0bc0cbbc66cc16439de8d031261d0af.js"
+  "/movie-search-react/precache-manifest.c6f440fecdf4921c348c3ab2250a43c0.js"
 );
 
 self.addEventListener('message', (event) => {
