89ccc2167167aa60f20b9b76f1681d5953f5aa4d
Andres Atehortua <anderson17colombia@hotmail.com>
2020-03-22 21:35:20 +0100

    Updates

---
+++ precache-manifest.08a316509bb70e7543321615ee8abfad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2885182750951a9b5c15654e8872400c",
+    "url": "/movie-search-reactjs/index.html"
+  },
+  {
+    "revision": "443641fea3d2054b3050",
+    "url": "/movie-search-reactjs/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b2d29eaf2f0d445d0fb9",
+    "url": "/movie-search-reactjs/static/css/main.46efebf3.chunk.css"
+  },
+  {
+    "revision": "443641fea3d2054b3050",
+    "url": "/movie-search-reactjs/static/js/2.6bfca580.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/movie-search-reactjs/static/js/2.6bfca580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2d29eaf2f0d445d0fb9",
+    "url": "/movie-search-reactjs/static/js/main.91effc7e.chunk.js"
+  },
+  {
+    "revision": "9d88428f9f0f71369de6",
+    "url": "/movie-search-reactjs/static/js/runtime-main.87aab3a9.js"
+  }
+]);
