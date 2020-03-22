d7e099cd17ba490dae891118b472895464ddca53
Andres Atehortua <anderson17colombia@hotmail.com>
2020-03-22 23:09:06 +0100

    aa

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ class App extends Component {
   _setResults = results => this.setState({ results })
 
   render() {
-    console.log(process.env.PUBLIC_URL)
     return (
       <>
         <Switch>
